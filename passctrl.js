e6be95f1502eba7b5bc380f1082c1994d8c6cd90
Juan Sotomayor <juan.sotomayor.ch@gmail.com>
2017-01-02 15:57:21 -0300

    Ajustes passctrl.js

--- passctrl.js
+++ passctrl.js
@@ -40,8 +40,6 @@ serialport.on('data', function(data){
         }else if(Data[1] == "ADMIN"){ // el caso que Data[1] = "ADMIN" significa que el admin va a verifiar o agregar usuario RFID 
            Code = Data[2]; //id bio 
            AddUser("rfid", Code, Data[3], Data[4]); //data3 = modulo  data4= iduser
-           //Entra a la función CheckRFID() en donde verifica al usuario y lo registra 
-                          
            //Data[2] Corresponde al serial unico del usuario para identificarlos
            //Data[3] Corresponde al ID de la etiqueta RFID, en hexadecimal
            //Data[4] Corresponde al numero del módulo de control de acceso en este caso al 1            
@@ -51,13 +49,13 @@ serialport.on('data', function(data){
         console.log("RBA");
         Access("KEYPAD", Data[1]);
     }else if(Data[0] == "ADMIN"){
-        console.log("Admin");
+        console.log("Acceso de administrado");
         CheckAdmin(Data[1], Data[2], Data[3]);
 
     }else if(Data[0] == "CHECK"){
         serialport.write("CHECK-");
-        console.log("CHECK");
-        user_biometria('1');
+        console.log("Chequeo comunición con módulo");
+        //user_biometria('1');
         //EstadoModulo(Data[1], Data[2]);
     }
     else if(Data[0] == "ADDUSER"){
@@ -125,8 +123,8 @@ function AddUser(tipo, codigo, modulo, idUser) {
     
     connection.end();
 }
-function user_biometria(x){
-    console.log("usuario biometria");
+function user_biometria(modulo){
+    console.log("chequear usuarios biometria");
     var mysql      = require('mysql');
     var connection = mysql.createConnection({
         host : host,
@@ -136,7 +134,7 @@ function user_biometria(x){
     });
 
     connection.connect();
-    var query = connection.query('SELECT code_biometria FROM usuarios_modulos WHERE idModulo ="'+x+'"', function(err, rows) {
+    var query = connection.query('SELECT code_biometria FROM usuarios_modulos WHERE idModulo ="'+modulo+'"', function(err, rows) {
         if (err) {
             console.log(error.message);
         } else {
@@ -147,8 +145,8 @@ function user_biometria(x){
                     bio_detec = bio_detec+"*"+rows[i].code_biometria;
                 }
             }
-            //serialport.write("FP_USERS*"+bio_detec+"-");
-            console.log("FP_USERS"+bio_detec+"-");
+            serialport.write("USERSBIO*"+bio_detec+"-");
+            console.log("USERSBIO"+bio_detec+"-");
         }
     });
     connection.end();
@@ -289,18 +287,22 @@ function CheckUser(idModulo, idUser){
             console.log(error.message);
         } else {
             console.log("Lista de usuarios de biometria");
+            var bio_detec = "";
             /***********************************aqui verificar el add user************* WHERE biometria ="1" */
             for(var a = 0; a<=125; a++){
                 var codigo = 0;
                 for (var i = 0; i < rows.length; i++) {
+                    bio_detec = bio_detec+"*"+rows[i].code_biometria;
                     if(rows[i].code_biometria){
                         if(rows[i].code_biometria == a){
                             codigo = 1;
                         }
                     }
                 }
+                serialport.write("USERSBIO*"+bio_detec+"-");
+                console.log("USERSBIO"+bio_detec+"-");
                 if(codigo == 0){
-                    id_bio = a;
+                    id_bio = a; //proximo usuario biometrico disponible
                     break;
                 }
             }
@@ -330,8 +332,8 @@ function CheckUser(idModulo, idUser){
                 if(code_rfid){
                     rfid=1;
                 }
-                console.log("USER_ADD*"+biometria+"*"+rfid+"*"+idUser+"*"+id_bio+"-");
-                serialport.write("USER_ADD*"+biometria+"*"+rfid+"*"+idUser+"*"+id_bio+"-");
+                console.log("USER_ADD*"+idUser+"*"+id_bio+"-");
+                serialport.write("USER_ADD*"+idUser+"*"+id_bio+"-");
             }else{
                 serialport.write("USER_DELETED-");
             }
